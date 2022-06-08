d8cf97119088de7bf9c8f0f8d1cc39ff733186bf
arnob-das <arnob.cse.20210104148@aust.edu>
2022-06-09 05:45:29 +0600

    login and registration form toggling done

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -6,13 +6,14 @@ import NavBar from '../NavBar/NavBar';
 import './Login.css'
 
 const Login = () => {
-    const { signInUsingGoogle, setError, handleRegisterWithEmailPassword, setUser,auth } = useAuth();
+    const { signInUsingGoogle, setError, handleRegisterWithEmailPassword, setUser, auth, error } = useAuth();
 
     // declare state
     let [fullName, setFullName] = useState("");
     let [photoUrl, setPhotoUrl] = useState("");
     let [email, setEmail] = useState("");
     let [password, setPassword] = useState("");
+    let [flag, setFlag] = useState(1); // 1 for registration
 
 
     const location = useLocation();
@@ -33,17 +34,34 @@ const Login = () => {
         setPassword(e.target.value);
     }
 
-    const handleRegistrationWithEmailPass = (e) => {
+    // handle flag
+    const handleFlag = () => {
+        if (flag === 0) {
+            setFlag(1);
+            return;
+        }
+        else {
+            setFlag(0);
+            return;
+        }
+    }
+
+    // handle user registration with email and password
+    const handleRegistrationLoginWithEmailPass = (e) => {
         e.preventDefault();
-        handleRegisterWithEmailPassword(auth,email, password)
+        // only for registration
+        flag && handleRegisterWithEmailPassword(auth, email, password)
             .then((userCredential) => {
                 const user = userCredential.user;
-                setUser(user);
+                //setUser(user);
+                setError("");
             })
             .catch((error) => {
                 const errorMessage = error.message;
                 setError(errorMessage)
             });
+
+        !flag && setError("Log is not processed");
 
     }
 
@@ -64,16 +82,20 @@ const Login = () => {
 
                 {/* for registration */}
                 <Form className="registration-form m-auto">
-                    <h3>Create An Account</h3>
-                    <Form.Group onBlur={handleFullName} className="mb-3">
-                        <Form.Label>Full Name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter Full Name" />
-                    </Form.Group>
+                    <h3>{flag ? 'Create An Account' : "Login"}</h3>
+                    {flag &&
+                        <div>
+                            <Form.Group onBlur={handleFullName} className="mb-3">
+                                <Form.Label>Full Name</Form.Label>
+                                <Form.Control onChange={handleFullName} type="text" placeholder="Enter Full Name" />
+                            </Form.Group>
 
-                    <Form.Group onBlur={handlePhotoUrl} className="mb-3">
-                        <Form.Label>Photo Url</Form.Label>
-                        <Form.Control type="email" placeholder="Enter Photo Url" />
-                    </Form.Group>
+                            <Form.Group onBlur={handlePhotoUrl} className="mb-3">
+                                <Form.Label>Photo Url</Form.Label>
+                                <Form.Control type="email" placeholder="Enter Photo Url" />
+                            </Form.Group>
+                        </div>
+                    }
 
                     <Form.Group onBlur={handleEmail} className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email</Form.Label>
@@ -85,12 +107,18 @@ const Login = () => {
                         <Form.Control type="password" placeholder="Enter Password" />
                     </Form.Group>
 
-                    <Button onClick={handleRegistrationWithEmailPass} variant="primary" type="submit" className="w-100">
-                        Create An Account
+                    {error &&
+                        <Form.Group onBlur={handlePassword} className="mb-3" controlId="formBasicPassword">
+                            <Form.Text className="text-danger">{error}</Form.Text>
+                        </Form.Group>
+                    }
+
+                    <Button onClick={handleRegistrationLoginWithEmailPass} variant="primary" type="submit" className="w-100">
+                        {flag ? "Create An Account" : "Login"}
                     </Button>
                 </Form>
                 <Form.Group className="d-flex justify-content-center mt-3">
-                    <p>Already Have An Account? <span className="text-blue text-underline">Login</span></p>
+                    <p>{flag ? "Already Have An Account?" : "New User?"} <span onClick={handleFlag} className="text-blue text-underline">{flag ? "Login" : "Register"}</span></p>
                 </Form.Group>
                 <Button onClick={handleGoogleSignIn}>Google Sign In</Button>
                 <Link to="/register">New User ?</Link>
